95a87a928402a5a8460c62083e28219712a75b94
Anney Park <anneypark@gmail.com>
2020-07-20 17:08:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  family = ["james", "lauren", "edward", "anney" ]
+  family = ["james", "lauren", "edward", "anney"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -24,7 +24,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
